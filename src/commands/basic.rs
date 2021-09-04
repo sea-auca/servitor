a5092b5d5e42af5fc6aadd8c089bcf4908c1654a
Alym Daniiarov <daniiarov_a@outlook.com>
2021-09-04 19:22:52 +0600

    Very basic and raw functional

---
+++ src/commands/basic.rs
@@ -0,0 +1,43 @@
+use serenity::framework::standard::{macros::command, CommandResult};
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use std::process::Command;
+
+const ABOUT_MSG: &str = "Hello! This is simple utility bot developed by our community. 
+    We are still in process of development and new features will be added later";
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn echo(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, &msg.content).await?;
+    Ok(())
+}
+
+#[command]
+async fn fortune(ctx: &Context, msg: &Message) -> CommandResult {
+    let output = Command::new("/usr/games/fortune")
+        .output()
+        .expect("Failed to execute fortune");
+    msg.channel_id
+        .say(&ctx.http, String::from_utf8_lossy(&output.stdout))
+        .await?;
+    Ok(())
+}
+
+#[command]
+async fn help(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "No help for you!").await?;
+    Ok(())
+}
+
+#[command]
+async fn about(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, ABOUT_MSG).await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,67 @@
 mod commands;
 
-use std::{env};
+use commands::basic::*;
 use serenity::{
     async_trait,
-//    client::bridge::gateway::ShardManager,
-    framework::{standard::macros::group, StandardFramework},
     client::bridge::gateway::GatewayIntents,
-    http::Http,
-    model::{event::ResumedEvent, gateway::Ready,},
+    //    client::bridge::gateway::ShardManager,
+    framework::{standard::macros::group, StandardFramework},
+    // http::Http,
+    model::{event::ResumedEvent, gateway::Ready, id::EmojiId, channel::{Reaction, ReactionType, Message}},
     prelude::*,
 };
+use std::env;
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("Connected as {}", ready.user.name);
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        println!("Resumed");
+    }
+    
+    async fn message(&self, ctx: Context, msg: Message) {
+        if &msg.content.chars().count() > &0 && &msg.content.chars().next().unwrap() == &'~'  {
+            return
+        }
+        if msg.content.contains("Яман") || msg.content.contains("яман") {
+            let reaction_type = ReactionType::Custom{animated: false, id: EmojiId(798454339134816256), name: Some(String::from(":shit_taster:"))};
+            if let Err(why) = msg.react(&ctx, reaction_type).await {
+                println!("Error reacting: {:?}", why);
+            }
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Курлык-курлык!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+        
+    }
+    
+    
+    
+}
+
+#[group]
+#[commands(ping, echo, fortune, help, about)]
+struct General;
+
 
 #[tokio::main]
 async fn main() {
     let token = env::var("BOT_TOKEN").expect("Bot token was not found");
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~"))
+        .group(&GENERAL_GROUP);
     let mut client = Client::builder(&token)
-        .intents(GatewayIntents::all());
+        .intents(GatewayIntents::all())
+        .framework(framework)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        print!("Client error: {:?}\n", why);
+    }
 }
