e55c99bb9451c0e4549130b4a32c691b9d6dc4e6
EHDW <artur.talkanbaev@gmail.com>
2021-09-05 16:39:53 +0600

    Added member add command

--- src/commands/basic_member.rs
+++ src/commands/basic_member.rs
@@ -1,14 +1,27 @@
 use serenity::framework::standard::{macros::command, macros::group, CommandResult};
-use serenity::model::{id::RoleId, prelude::*};
+use serenity::model::{guild::Guild, id::RoleId, prelude::*};
 use serenity::prelude::*;
 
 #[group]
-#[commands(make_me_a_member)]
+#[commands(member_me)]
 struct BasicMember;
 
 #[command]
-async fn make_me_a_member(ctx: &Context, msg: &Message) -> CommandResult {
+async fn member_me(ctx: &Context, msg: &Message) -> CommandResult {
     let member_role_id: RoleId = RoleId(884006016767889419);
-    println!("made member {:#?}", msg);
+    if let Some(guild_id) = msg.guild_id {
+        if let Some(guild) = guild_id.to_guild_cached(&ctx).await {
+            if let Ok(mut member) = guild.member(&ctx, &msg.author.id).await {
+                match member.add_role(&ctx, &member_role_id).await {
+                    Ok(_) => {
+                        println!("Added basic member role");
+                    }
+                    Err(err) => {
+                        println!("Error ocurred: {:#?}", err);
+                    }
+                };
+            }
+        }
+    }
     Ok(())
 }
