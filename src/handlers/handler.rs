c71ac43c151f755be54bdace0cad12d4307af05c
Alym Daniiarov <daniiarov_a@outlook.com>
2021-10-09 09:28:21 +0600

    Implemented roles reactions handling

--- src/handlers/handler.rs
+++ src/handlers/handler.rs
@@ -10,7 +10,7 @@ use serenity::{
     },
     prelude::*,
 };
-use crate::global::shared::LOGGER;
+use crate::global::shared::{LOGGER, BOT_DATABASE};
 use crate::logging::Level;
 
 pub struct Handler;
@@ -27,16 +27,21 @@ impl EventHandler for Handler {
     
     //welp we will do it in appropritate way later
     async fn reaction_add(&self, ctx: Context, add_reaction: Reaction) {
-        if add_reaction.message_id != MessageId(884019412376637442) {
+        let msg_id = add_reaction.message_id.0;
+        let mut role_id = String::new();
+        if let ReactionType::Custom{animated:_animation, id: emoji_id, name: _name} = add_reaction.emoji {
+            BOT_DATABASE.lock().unwrap().get_role_id(msg_id,emoji_id.0,&mut role_id);
+        }
+        else {
             return
         }
-    
+        let role = role_id.parse::<u64>().unwrap();
         if let Some(guild_id) = add_reaction.guild_id {
             if let Some(guild) = guild_id.to_guild_cached(&ctx).await {
                 match add_reaction.user_id {
                     Some(user_id) =>  {
                         if let Ok(mut member) = guild.member(&ctx, &user_id).await {
-                        match member.add_role(&ctx, RoleId(883626459980247060)).await {
+                        match member.add_role(&ctx, RoleId(role)).await {
                             Ok(_) => {
                                 println!("Added basic member role");
                             }
--- src/utilities/db.rs
+++ src/utilities/db.rs
@@ -15,6 +15,20 @@ impl Client {
         self.connection = Some(Connection::open(path).expect("Error connecting to database"));
     }
     
+    pub fn get_role_id(&mut self, msg_id: u64, emoji_id: u64, result: &mut String) {
+        let mut query = self.connection
+            .as_ref().unwrap()
+            .prepare("SELECT role_id FROM roles_reactions WHERE msg_id = :msg_id AND emoji_id = :emoji_id").expect("Error");
+        let mut rows = query.query(&[(":msg_id", msg_id.to_string().as_str()),(":emoji_id", 
+            emoji_id.to_string().as_str())])
+            .expect("Error");
+        *result = String::from("No role");
+        while let Some(row) = rows.next().expect("Error") {
+            *result = row.get(0)
+                .expect("Error");
+        }    
+    }
+    
     pub fn test_query(&mut self) -> String {
         let mut statement = self.connection.as_ref().unwrap().prepare("SELECT bar FROM foo").expect("Error");
         let mut rows = statement.query([]).expect("Error");
